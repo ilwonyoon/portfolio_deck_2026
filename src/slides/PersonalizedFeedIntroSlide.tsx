77ec064164f11bd56f0d1723d6694f2af71dd174
IlwonYoon <ilwonyoon@gmail.com>
2026-05-01 13:36:47 -0700

    Reload feed video when source changes

--- src/slides/PersonalizedFeedIntroSlide.tsx
+++ src/slides/PersonalizedFeedIntroSlide.tsx
@@ -15,6 +15,7 @@ export function PersonalizedFeedIntroSlide({
     <article className="personalized-feed-intro-slide" data-node-id="6133:90440">
       <div className="personalized-feed-intro-slide__phone" data-node-id="6133:90657">
         <video
+          key={`${videoWebm}:${videoMp4}`}
           className="personalized-feed-intro-slide__video"
           autoPlay
           loop
